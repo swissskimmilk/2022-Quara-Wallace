23b82e8b0c6c6558e6ba29009e3bf60c13209865
unknown <aidanrickert@comcast.net>
2022-02-18 17:41:48 -0800

    need fix constants

--- src/main/java/frc/robot/commands/SnapRight.java
+++ src/main/java/frc/robot/commands/SnapRight.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
@@ -9,16 +10,14 @@ import java.lang.Math;
 import java.text.DecimalFormat;
 
 public class SnapRight extends CommandBase {
-  private double error;
 
   private double initAngle;
   private double newAngle; 
   private double lastAngle;
-  private double integralSum;
 
   private IMU subsysIMU;
   private Drivetrain drivetrain;
-  private double turnRate;
+  private PIDController pid;
 
   public SnapRight(Drivetrain mDrivetrain, IMU mIMU) {
     drivetrain = mDrivetrain;
@@ -31,6 +30,15 @@ public class SnapRight extends CommandBase {
   public void initialize() {
     System.out.println("Snap Right");
     DecimalFormat angleFormat = new DecimalFormat("###.##");
+    
+    // these be fucked
+    double kP = 0.02; 
+    double kD = 10;
+    double kI = 0;
+
+    pid = new PIDController(kP, kI, kD);
+    pid.setTolerance(Constants.angleTolerance);
+    pid.enableContinuousInput(0, 360);
 
     // angle bounded from 0 to 360 including negatives
     initAngle = (subsysIMU.getAngle() % 360 + 360) % 360;
@@ -44,13 +52,12 @@ public class SnapRight extends CommandBase {
     } else if (initAngle > 90) {
       newAngle = 90;
     } else {
-      newAngle = 0;
+      newAngle = 360;
     }
     System.out.println("Turning to: " + angleFormat.format(newAngle));
     System.out.println();
 
     // multiply the speed by this to make it range from 1 to 0 (ish) 
-    turnRate = 1 / (newAngle - initAngle);
     lastAngle = subsysIMU.getAngle();
   }
 
@@ -60,17 +67,9 @@ public class SnapRight extends CommandBase {
     // angle confined from 0 to 360 including negatives
     double currAngle = (subsysIMU.getAngle() % 360 + 360) % 360;
 
-    // PID vars
-    error = newAngle - currAngle;
-    integralSum += error * 0.02;
-    double derivative = (lastAngle - currAngle) / 0.02;
-
-    // PID calculation 
-    double movement = Constants.PMult * error + Constants.IMult * integralSum + Constants.DMult * derivative;
-
-    System.out.println("Err: " + error + " I: " + integralSum + " D: " + derivative + " Mvmt: " + movement);
-    // speed dependent on angle distance
-    RobotContainer.myRobot.arcadeDrive(movement, 0);
+    // speed dependent on angle distance\
+    System.out.println(pid.calculate(newAngle, currAngle));
+    RobotContainer.myRobot.arcadeDrive(pid.calculate(newAngle, currAngle), 0);
 
     lastAngle = currAngle;
   }
@@ -83,7 +82,7 @@ public class SnapRight extends CommandBase {
   @Override
   public boolean isFinished() {
     // end if the angle is approximately the desired one
-    if (Math.abs(error) <= Constants.errorTolerance) {
+    if (pid.atSetpoint()) {
       return true;
     } 
     else {
